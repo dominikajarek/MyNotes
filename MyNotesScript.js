ab7543696463d747558662212af89052713eb281
domi-dominika <dominika.jarek18@gmail.comgit config --global user.name Dominika4git config --global user.email dominika.jarek18@gmail.com>
2020-08-10 10:49:22 +0200

    drag&drop works, new background, pretty button

--- MyNotesScript.js
+++ MyNotesScript.js
@@ -1,16 +1,38 @@
-const addNote = document.querySelector(".note");
 let id = 0;
+const addNote = document.querySelector(".noteCell");
+const addNotes = document.querySelector(".note");
+const remove = document.getElementsByClassName("noteCell");
+
 
 document.getElementById("add").addEventListener("click", function () {
     let clone = addNote.cloneNode(true);
     clone.id = "note " + id++;
-    document.body.appendChild(clone);
+    addNotes.appendChild(clone);
     addNote.after(clone);
 })
 
-document.getElementById("remove").addEventListener("click", function () {
-    let remove = document.getElementById("input");
-    let removeNote = document.getElementById("input1");
-    remove.remove();
-    removeNote.remove();
+$(addNotes).on('click', '#btn', function () {
+    if (remove.length  > 1) {
+        $(this).closest('.noteCell').remove();
+    }
 })
+
+function drag(event) {
+    let style = window.getComputedStyle(event.target, null);
+    let str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY) + ',' + event.target.id;
+    event.dataTransfer.setData("Text", str);
+}
+
+function drop(event) {
+    let offset = event.dataTransfer.getData("Text").split(',');
+    let dm = document.getElementById(offset[2]);
+    dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+    event.preventDefault();
+    return false;
+}
+
+function dragOver(event) {
+    event.preventDefault();
+    return false;
+}
